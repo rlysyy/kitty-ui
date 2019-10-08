7519e47b3aff469397b20d6705659cfed8028ab6
rlysyy <rlysyy@hotmail.com>
2019-10-08 20:08:31 +0800

    element-ui 测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,7 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
+      <el-button type="primary">主要按钮</el-button>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
